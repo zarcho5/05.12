d6ce65f5e002727517c92ef78a798010be073d92
zarcho5 <den812008@gmail.com>
2025-12-05 11:03:13 +0300

    zadaniye2у

--- script.js
+++ script.js
@@ -7,6 +7,45 @@ console.log(containsChar("Hello", "H"));
 
 //Задание 2
 
-const reverse = (str) => str.split('').reverse().join('');
+const reverse = (str) => {
+    str.split('').reverse().join('');
+}
 
 console.log(reverse("hello"));
+
+//Задание 3
+
+const wordsToRegister = (str) => {
+    
+    for (let i = 0; i < str.length; i++) {
+        if (i % 2 === 0) {
+            result += str[i].toLowerCase();
+        } else {
+            result += str[i].toUpperCase();
+        }
+    }
+    return result;
+}
+
+//Задание 4
+
+const sumOfDigitsInString = (str) => {
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (char >= '0' && char <= '9') {
+            sum += parseInt(char, 10);
+        }
+    }
+    return sum;
+};
+
+//Задание 5
+
+const checkChars = (str, what, onWhat) =>{
+    if (!str.includes(what)) {
+        return 'Error';
+    }
+
+    return str.replace(what, onWhat);
+}
